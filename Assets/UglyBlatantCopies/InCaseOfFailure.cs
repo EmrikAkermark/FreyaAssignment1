6a9e42869edf367bd11edac5e2b2e4cc78b9f268
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-25 19:39:38 +0200

    I might be done actually

---
+++ Assets/UglyBlatantCopies/InCaseOfFailure.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InCaseOfFailure : MonoBehaviour
+{
+    public Text Word1, Word2;
+    public float SwitchColourTime = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SwitchColours());
+    }
+
+    private IEnumerator SwitchColours()
+    {
+        while(true)
+        {
+            Word1.color = Color.black;
+            Word2.color = Color.white;
+            yield return new WaitForSeconds(SwitchColourTime);
+            Word1.color = Color.white;
+            Word2.color = Color.black;
+            yield return new WaitForSeconds(SwitchColourTime);
+        }
+    }
+}
---
+++ Assets/UglyBlatantCopies/WorldToLocal.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class WorldToLocal : MonoBehaviour
+{
+    public Transform ParentTransform, ChildTransform, WorldTransform;
+    public Vector2 LocalToWorldOffset, ChildWorldPosition;
+    public Vector2 ParentToWorldVector, ParentToWorldLocalVector, WorldAndLocalMagnitudes;
+
+    public Text LocalToWorldPosition, WorldToLocalPosition;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        FindWorldPosition();
+        FindLocalPosition();
+
+
+    }
+
+    private void FindWorldPosition()
+    {
+        //To find the world position of the child object, first we have to figure out the
+        //offset the Parent Transform has introduced. By multiplying the childs localPosition
+        //with the parent's world rotation, we can find this offset
+        LocalToWorldOffset = ParentTransform.right * ChildTransform.localPosition.x + ParentTransform.up * ChildTransform.localPosition.y;
+        //To get the child's world position, we simply add the offset to the parent's
+        //world position! The offset takes care of any rotation the parent might have.
+        ChildWorldPosition = new Vector2(ParentTransform.position.x + LocalToWorldOffset.x, ParentTransform.position.y + LocalToWorldOffset.y);
+        LocalToWorldPosition.text = $"The local positions world coordinates are {ChildWorldPosition.x}, {ChildWorldPosition.y}";
+    }
+
+    private void FindLocalPosition()
+    {
+        //This finds the vector between the "local" point and the tracked point
+        ParentToWorldVector = WorldTransform.position - ParentTransform.position;
+
+        //This doesn't take the local positions rotation in consideration.
+        //However, with the dot product, we can!
+        //Since transform.right and transform.up alway return normalised vectors,
+        //we can use them to find where where the Parent to World vector
+        //intersects with the Parent.right and Parent.up!
+        ParentToWorldLocalVector.x = Vector2.Dot(ParentTransform.right, ParentToWorldVector);
+        ParentToWorldLocalVector.y = Vector2.Dot(ParentTransform.up, ParentToWorldVector);
+        WorldToLocalPosition.text = $"The world positions local coordinates are {ParentToWorldLocalVector.x}, {ParentToWorldLocalVector.y}";
+
+
+        //I made this for myself, if both magnitudes are equal, the I've done it right.
+        WorldAndLocalMagnitudes.x = ParentToWorldVector.magnitude;
+        WorldAndLocalMagnitudes.y = ParentToWorldLocalVector.magnitude;
+
+        //I wrote all this out to prove to myself that I understood what these things
+        //do and that I didn't just blindly follow Freyas instructions. I had to look
+        //at the stream and now I feel guilty.
+
+        //Please don't make me hook this up to gizmos, I don't know how they work.
+    }
+
+    
+
+#endif
+}
